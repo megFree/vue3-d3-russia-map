af647e124cd19b3428db6798dd9bd7c0d02ee539
Sergey Grechishkin <meg.work.996@gmail.com>
2024-03-04 03:43:18 +0300

    added gh-pages

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,6 @@ module.exports = defineConfig({
         additionalData: `@import '~@/assets/styles/variables.scss'; @import '~@/assets/styles/fonts.scss'; @import '~@/assets/styles/base.scss';`
       }
     }
-  }
+  },
+  publicPath: 'https://megfree.github.io/vue3-d3-russia-map/',
 })
